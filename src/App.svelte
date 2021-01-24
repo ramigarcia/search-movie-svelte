de30cf80ca5b621ca8b25e871111c23a43d94425
Ramiro Garcia <rg2607836@gmail.com>
2021-01-24 15:41:33 -0300

    Contador reutilizando componentes

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,7 @@
 <script>
-	export let saludo;
+	import Counter from './counter.svelte'
 </script>
 
-<main>
-	<h1>{saludo}</h1>
-	<p>Visit <a href="https://ramigarcia.github.io/portafolio-2021" target="_blank">My Portfolio</a></p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #09f;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<Counter initialCounter={2} />
+<Counter initialCounter={8} />
+<Counter initialCounter={20} />
---
+++ src/counter.svelte
@@ -0,0 +1,42 @@
+<script>
+	export let initialCounter = 0;
+	let contador = initialCounter
+
+	function incrementar (){
+		contador ++
+	}
+
+	function reset(){
+		contador = 0
+	}
+</script>
+
+<main>
+	<span>{contador}</span>
+	<br>
+	<button on:click={incrementar}>Increment</button>
+	<button on:click={reset}>Reset</button>
+	<p>Visit <a href="https://ramigarcia.github.io/portafolio-2021" target="_blank">My Portfolio</a></p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	span {
+		color: #09f;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
